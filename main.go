3e4b8fb5f09b4c96c0a8170ad704c8e1dce0dece
arxaqapi <arxaqapi@protonmail.com>
2019-09-13 16:32:11 +0200

    added simple go server

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func homePageHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("./static/index.html"))
+	tmpl.Execute(w, nil)
+}
+
+func main() {
+	http.HandleFunc("/", homePageHandler)
+
+	http.Handle("/static/",
+		http.StripPrefix("/static/",
+			http.FileServer(http.Dir("static"))))
+
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
