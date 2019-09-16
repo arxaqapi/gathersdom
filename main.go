e61e662d1f2dfb33d7b2b54eab2d5e935190c061
arxaqapi <arxaqapi@protonmail.com>
2019-09-16 21:11:37 +0200

    index.html moved

--- main.go
+++ main.go
@@ -7,17 +7,17 @@ import (
 )
 
 func homePageHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("./static/index.html"))
+	tmpl := template.Must(template.ParseFiles("index.html"))
 	tmpl.Execute(w, nil)
 }
 
 func main() {
 	http.HandleFunc("/", homePageHandler)
-
-	http.Handle("/static/",
-		http.StripPrefix("/static/",
-			http.FileServer(http.Dir("static"))))
-
+	/*
+		http.Handle("/static/",
+			http.StripPrefix("/static/",
+				http.FileServer(http.Dir("static"))))
+	*/
 	err := http.ListenAndServe(":8080", nil)
 
 	if err != nil {
